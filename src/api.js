37a1707140360af40dd4bef3baa7fa57c74edf34
camerona56 <cameronadamson56@gmail.com>
2022-12-19 11:29:26 +0000

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '94bcdc57d8msh4f00780558c6c2bp1012cfjsn141791b771b6',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "edf99338214874b326e43564e89f9149";
